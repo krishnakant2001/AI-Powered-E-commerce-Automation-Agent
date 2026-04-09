4f615eb297da7f1f590689d14a2b877ce39348cd
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2026-04-09 19:41:32 +0530

    Initiate the user service

---
+++ user-service/src/main/java/com/strikerkk/aicommerce/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.strikerkk.aicommerce.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/strikerkk/aicommerce/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strikerkk.aicommerce.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
