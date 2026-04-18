00a8c3ab028cacb1a121ff7620fca158cb7d3479
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2026-04-18 11:23:59 +0530

    Initiate the cart service

---
+++ cart-service/src/test/java/com/strikerkk/aicommerce/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strikerkk.aicommerce.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
