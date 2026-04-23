cf174726a0c0523c6ef9f6f3acbb314c0338f80b
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2026-04-23 08:29:05 +0530

    Initiate the payment service

---
+++ payment-service/src/test/java/com/strikerkk/aicommerce/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strikerkk.aicommerce.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
