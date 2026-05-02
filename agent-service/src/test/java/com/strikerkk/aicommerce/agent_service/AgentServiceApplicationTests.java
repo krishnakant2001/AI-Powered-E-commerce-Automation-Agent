d6b9f90be694b0bf4618e7e5ad60a5ef341b0535
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2026-05-02 21:14:24 +0530

    Initiate the agent service for AI workflow

---
+++ agent-service/src/test/java/com/strikerkk/aicommerce/agent_service/AgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strikerkk.aicommerce.agent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
