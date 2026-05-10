504fec94369d381b18cbb1b3d2797fa9497c2493
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2026-05-10 10:49:55 +0530

    LLM api calling and handle request response

--- agent-service/src/main/java/com/strikerkk/aicommerce/agent_service/service/AgentChatService.java
+++ agent-service/src/main/java/com/strikerkk/aicommerce/agent_service/service/AgentChatService.java
@@ -1,12 +1,17 @@
 package com.strikerkk.aicommerce.agent_service.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.strikerkk.aicommerce.agent_service.auth.UserContext;
 import com.strikerkk.aicommerce.agent_service.dto.request.ChatRequest;
 import com.strikerkk.aicommerce.agent_service.dto.request.StartSessionRequest;
 import com.strikerkk.aicommerce.agent_service.dto.response.ChatResponse;
+import com.strikerkk.aicommerce.agent_service.dto.summary.ToolCallSummary;
 import com.strikerkk.aicommerce.agent_service.entity.AgentSession;
 import com.strikerkk.aicommerce.agent_service.entity.enums.MessageRole;
 import com.strikerkk.aicommerce.agent_service.exception.AgentException;
+import com.strikerkk.aicommerce.agent_service.llm.SystemPromptBuilder;
+import com.strikerkk.aicommerce.agent_service.llm.ToolDefinitionBuilder;
 import com.strikerkk.aicommerce.agent_service.model.ConversationMessage;
 import com.strikerkk.aicommerce.agent_service.model.SessionContext;
 import com.strikerkk.aicommerce.agent_service.repository.AgentSessionRepository;
@@ -14,6 +19,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.*;
 
 @Service
@@ -24,28 +33,122 @@ public class AgentChatService {
     private final AgentSessionRepository agentSessionRepository;
     private final SessionContextService sessionContextService;
     private final AgentSessionService agentSessionService;
+    private final SystemPromptBuilder systemPrompt;
+    private final ToolDefinitionBuilder toolDefinition;
+    private final ObjectMapper objectMapper;
+
+    private String llmKey;
+    private String model;
+    private String maxTokens;
+
+    private static final int MAX_TOOL_CALLS = 10;
+
+    private static final String LLL_URL = "";
 
     public ChatResponse chat(ChatRequest request) {
 
         Long userId = Long.valueOf(UserContext.getUserId());
-
         log.info("Processing chat for userId={} sessionId={}", userId, request.getSessionId());
 
+        // Step 1: Resolve session
         AgentSession session = resolveSession(userId, request.getSessionId());
         UUID sessionId = session.getSessionId();
 
+        // Step 2: Load SessionContext from Redis
         SessionContext sessionContext = sessionContextService.findBySessionId(sessionId)
                 .orElseGet(() -> rebuildContextFromPostgres(session, userId));
 
+        // Step 3: Append user message to Redis context
         ConversationMessage newUserMessage = ConversationMessage.builder()
                 .role("user")
                 .content(request.getMessage())
                 .build();
         sessionContext.getConversationMessages().add(newUserMessage);
 
+        // Save user message to postgres
         agentSessionService.saveMessage(session, MessageRole.USER, request.getMessage(), null, null, null);
 
 
+        // Step 4: LLM orchestration loop
+        String finalResponse = null;
+        List<ToolCallSummary> toolCallSummaries = new ArrayList<>();
+        int loopCount = 0;
+
+        while(loopCount < MAX_TOOL_CALLS) {
+            loopCount++;
+            log.info("LLM loop iteration {} for sessionId={}", loopCount, sessionId);
+
+            // Call API
+            String llmResponseRaw = callLlmApi(sessionContext.getConversationMessages());
+            JsonNode llmResponse = parseJson(llmResponseRaw);
+        }
+
+    }
+
+    // ——————————————————————————————————
+    // Call LLM API
+    // ——————————————————————————————————
+
+    private String callLlmApi(List<ConversationMessage> history) {
+        try {
+            // Build messages array from conversation history
+            List<Object> messages = buildMessagesForApi(history);
+
+            // Build request body
+            String requestBody = objectMapper.writeValueAsString(new HashMap<>() {{
+                put("model", model);
+                put("max_tokens", maxTokens);
+                put("system", systemPrompt.build());
+                put("tools", toolDefinition.build());
+                put("messages", messages);
+            }});
+
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(LLL_URL))
+                    .header("Content-Type", "application/json")
+                    .header("x-api-key", llmKey)
+                    .header("llm-version", "2023-06-01")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() != 200) {
+                log.error("Claude API returned status={} body={}", response.statusCode(), response.body());
+                throw new AgentException("LLM api error: " + response.statusCode());
+            }
+
+            return response.body();
+
+        } catch (AgentException agentEx) {
+            throw agentEx;
+        } catch (Exception ex) {
+            throw new AgentException("Failed to call LLM api", ex);
+        }
+    }
+
+    private List<Object> buildMessagesForApi(List<ConversationMessage> history) {
+        List<Object> messages = new ArrayList<>();
+
+        for(ConversationMessage msg : history) {
+            if("tool_result".equals(msg.getToolName())) {
+                messages.add(Map.of(
+                        "role", "user",
+                        "content", msg.getContent()
+                ));
+            }
+            else {
+                messages.add(Map.of(
+                        "role", msg.getRole(),
+                        "content", msg.getContent() != null ? msg.getContent() : ""
+                ));
+            }
+        }
+
+        return messages;
     }
 
     // Resolve session - find existing session by using sessionId and userId
@@ -96,6 +199,14 @@ public class AgentChatService {
 
         sessionContextService.save(context);
         return context;
+    }
+
+    private JsonNode parseJson(String json) {
+        try {
+            return objectMapper.readTree(json);
+        } catch (Exception ex) {
+            throw new AgentException("Failed to parse LLM response as json ", ex);
+        }
     }
 
 }
